c315dc0f8b0ea949ee146e7de2ab63ca6c3ca2f0
charybdis1 <email@example.com>
2022-11-13 01:30:37 +0600

    шпора а еще кликлистенер вон

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,69 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.graphics.component1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Из текстовой XML разметки создаются видмые на устройстве View
         setContentView(R.layout.activity_main)
+
+        // Присвоить переменной ссылку на view с id button1 из layout activity_main
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
+        val button6 = findViewById<Button>(R.id.button6)
+        val button7 = findViewById<Button>(R.id.button7)
+        val button8 = findViewById<Button>(R.id.button8)
+        val button9 = findViewById<Button>(R.id.button9)
+        val button0 = findViewById<Button>(R.id.button_zero)
+        val buttonMinus = findViewById<Button>(R.id.button_minus)
+        val buttonPlus = findViewById<Button>(R.id.button_plus)
+        val buttonDiv = findViewById<Button>(R.id.button_div)
+        val buttonMult = findViewById<Button>(R.id.button_mult)
+        val buttonEquals = findViewById<Button>(R.id.button_equals)
+        val buttonC = findViewById<Button>(R.id.button_C)
+        val textView = findViewById<TextView>(R.id.textView)
+
+
+        // Присваиваем переменной ссылку на экземпляр класса (объект)
+        val clickListener = object : View.OnClickListener {
+
+            // Метод объекта OnClickListener вызовется при нажатии на кнопку
+            override fun onClick(view: View) {
+                // В переменную digit присвой значение цифры в зависимости от нажатой кнопки
+                // view - кнопка из XML, на которую нажали
+                val digit = when (view.id) {
+                    R.id.button1 -> 1
+                    R.id.button2 -> 2
+                    R.id.button3 -> 3
+                    R.id.button4 -> 4
+                    R.id.button5 -> 5
+                    R.id.button6 -> 6
+                    R.id.button7 -> 7
+                    R.id.button8 -> 8
+                    R.id.button9 -> 9
+                    R.id.button_zero -> 0
+
+                    else -> -0
+                }
+
+                textView.text = digit.toString()
+            }
+
+
+        }
+
+        // Передаем ссылку на объект clickListener в метод setOnClickListener,
+        // который назначит его кнопке
+        button1.setOnClickListener(clickListener)
+
     }
 }
---
+++ app/src/main/java/com/example/cheatsheet/BasicStructure.kt
@@ -0,0 +1,120 @@
+package com.example.cheatsheet
+
+// Конструкции языка в порядке усложнения
+
+// 1
+// Переменные примитивных (простых) типов.
+// Передаются по значению - значение каждый раз копируется.
+
+// Финальные, в java - final
+val intValue = 1
+val doubleValue = 2.3
+val booleanValue = true
+
+// Изменямые переменные, mutable
+var intVariable = 4
+
+
+// 2
+// Функции
+
+// Без параметров, без возврата значения
+fun printString() {
+    println("doSomething")
+}
+
+// Без параметров, с возвратом значения
+fun calculate(): Int {
+    println("start calculating")
+    val c = intValue + intValue // тело функции
+    return c // возврат значения
+}
+
+// C параметром, с возвратом значения
+fun add(n: Int, m: Int): Int {
+    return n + m
+}
+
+// 3
+// Использование переменных и функций в различных сочетаниях
+
+// Возврат значения функции
+// Присвоение переменной результата работы функции,
+// в переменной будет возвращаемое значение функции - return
+fun testReturns() {
+    println("before calculate fun call")
+    val a = calculate()
+    println("after calculate fun call, returned value is $a")
+}
+
+// Передача переменных в параметры функций
+val addResult = add(intValue, 1)
+
+// Использование переменных и функций в вычислениях
+val sumFunVal = calculate() + 1
+
+
+// 4
+// Использование переменных и фунций в условиях и операторах.
+// Вместо переменной всегда можно подставить функцию и наоборот.
+fun conditionExample() {
+    if (calculate() > 6 && booleanValue) {
+        println("Calculate correct")
+    }
+}
+
+fun whenExample() {
+    when (intValue) {
+        1 -> println("Equals one")
+        2 -> println("Equals two")
+        else -> {
+            println("Nothing matches")
+            println("Exiting")
+        }
+    }
+}
+
+
+// 5
+// Классы - структуры языка программирования
+// для описания сущностей реального мира
+// или сущностей в программировании (listener, view)
+// путем объединения полей и методов под одним именем.
+// Классы могут содержать поля (переменные) и методы (функции).
+
+class User {
+    var name = "Ivan"
+    var surname = "Ivanov"
+    var inn = 1234567890
+
+    fun getShortName(): String {
+        return name.first() + ". " + surname
+    }
+}
+
+// Класс это описание в коде, экземпляр класса это информация в памяти устройства.
+// Чтобы создать экемпляр класса нужно вызвать конструктор,
+// сейчас он пустой - не содержит параметров.
+
+val user = User()
+
+//  ^ переменная - хранит ссылку на экемпляр класса User
+//         ^ создание экземпляра класса
+
+// Можно обращаться к свойствам (полям (переменным)) и методам класса через переменную
+fun printUserName() {
+    println(user.name)
+}
+
+fun printUserShortName() {
+    println(user.getShortName())
+}
+
+// Тестировать код можно здесь
+fun main() {
+    println("start main fun")
+    //printUserShortName()
+    //testReturns()
+
+    println(user)
+}
